1527f5beff5a3a241fed30e34ffb7ff3ba823fc3
Rafał Krzewski <rafal.krzewski@caltha.pl>
2013-05-05 13:58:52 +0200

    a helper function for parsing operators

--- ldapfilters/src/main/java/pl/caltha/labs/ldapfilters/Operator.java
+++ ldapfilters/src/main/java/pl/caltha/labs/ldapfilters/Operator.java
@@ -3,5 +3,15 @@ package pl.caltha.labs.ldapfilters;
 public enum Operator {
 	EQUAL, APPROX, GREATER_EQ, LESS_EQ, PRESENT, SUBSTRING,
 	/** Only applicable to {@link AttributeType#REQUIREMENT} */
-	MATCHES
+	MATCHES;
+
+	private static String[] repr = { "=", "~=", ">=", "<=", "=*" };
+
+	public static Operator parse(String str, int pos) {
+		for (int i = 0; i < repr.length; i++) {
+			if(repr[i].equals(str))
+				return Operator.values()[i];
+		}
+		throw new ParseException("invalid operator at position " + pos);
+	}
 }
